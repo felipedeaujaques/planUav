928829fe4c81cb71328a525fa156dfcca438635e
felipedeaujaques <felipedeaujaques@users.noreply.github.com>
2018-11-13 20:18:37 +0100

    Structural changes to calculate numbers of pictures and velocity

--- flyLib.js
+++ flyLib.js
@@ -1,53 +1,80 @@
 "use strict";
 
-class FlightArea {
-	constructor(xValues,yValues){
+function FlightArea(xValues,yValues) {
 		this.xValues = xValues;
 		this.yValues = yValues;
-	};
-	getLowerLeft() {		
+		this.maxLength = function () {
+										var R = 6378.137; // Radius of earth in KM
+										var storage = [];
+										//handling of distance calculation between first and last element in array
+										var dLat = this.yValues[3] * Math.PI / 180 - this.yValues[0] * Math.PI / 180;
+										var dLon = this.xValues[3] * Math.PI / 180 - this.xValues[0] * Math.PI / 180;
+										var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+												Math.cos(this.yValues[0] * Math.PI / 180) * Math.cos(this.yValues[3] * Math.PI / 180) *
+												Math.sin(dLon/2) * Math.sin(dLon/2);
+										var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+										var d = R * c * 1000; //from km to meters
+										storage.push(d);
+										xValues = this.xValues;
+									for (var i = 1; i < xValues.length;i++) {
+										dLat = this.yValues[i] * Math.PI / 180 - this.yValues[i-1] * Math.PI / 180;
+										dLon = this.xValues[i] * Math.PI / 180 - this.xValues[i-1] * Math.PI / 180;
+										a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+												Math.cos(this.yValues[i-1] * Math.PI / 180) * Math.cos(this.yValues[i] * Math.PI / 180) *
+												Math.sin(dLon/2) * Math.sin(dLon/2);
+										c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+										d = R * c * 1000; //from km to meters
+										storage.push(d);
+									}
+										return Math.max(...storage);
+									};
+		this.minLength = function () {
+									var R = 6378.137; // Radius of earth in KM
+									var storage = [];
+									//handling of distance calculation between first and last element in array
+									var dLat = this.yValues[3] * Math.PI / 180 - this.yValues[0] * Math.PI / 180;
+									var dLon = this.xValues[3] * Math.PI / 180 - this.xValues[0] * Math.PI / 180;
+									var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+											Math.cos(this.yValues[0] * Math.PI / 180) * Math.cos(this.yValues[3] * Math.PI / 180) *
+											Math.sin(dLon/2) * Math.sin(dLon/2);
+									var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+									var d = R * c * 1000; //from km to meters
+									storage.push(d);
+								for (var i = 1; i < xValues.length;i++) {
+									dLat = this.yValues[i] * Math.PI / 180 - this.yValues[i-1] * Math.PI / 180;
+									dLon = this.xValues[i] * Math.PI / 180 - this.xValues[i-1] * Math.PI / 180;
+									a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+											Math.cos(this.yValues[i-1] * Math.PI / 180) * Math.cos(this.yValues[i] * Math.PI / 180) *
+											Math.sin(dLon/2) * Math.sin(dLon/2);
+									c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+									d = R * c * 1000; //from km to meters
+									storage.push(d);
+								}
+									return Math.min(...storage);
+								};
+	this.LowerLeft = function () {		
 						var lowerLeft = [Math.min(...this.xValues),Math.min(...this.yValues)];
 						return lowerLeft;
 	};
-	getUpperRight()	{
+	this.UpperRight = function ()	{
 						var upperRight = [Math.max(...this.xValues),Math.max(...this.yValues)];
 						return upperRight;
 					};
-	getLongestSide(){
-						var R = 6378.137; // Radius of earth in KM
-						var storage = [];
-						//handling of distance calculation between first and last element in array
-						var dLat = this.yValues[3] * Math.PI / 180 - this.yValues[0] * Math.PI / 180;
-						var dLon = this.xValues[3] * Math.PI / 180 - this.xValues[0] * Math.PI / 180;
-						var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-								Math.cos(this.yValues[0] * Math.PI / 180) * Math.cos(this.yValues[3] * Math.PI / 180) *
-								Math.sin(dLon/2) * Math.sin(dLon/2);
-						var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-						var d = R * c * 1000; //from km to meters
-						storage.push(d);
-					for (var i = 1; i < xValues.length;i++) {
-						dLat = this.yValues[i] * Math.PI / 180 - this.yValues[i-1] * Math.PI / 180;
-						dLon = this.xValues[i] * Math.PI / 180 - this.xValues[i-1] * Math.PI / 180;
-						a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-								Math.cos(this.yValues[i-1] * Math.PI / 180) * Math.cos(this.yValues[i] * Math.PI / 180) *
-								Math.sin(dLon/2) * Math.sin(dLon/2);
-						c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-						d = R * c * 1000; //from km to meters
-						storage.push(d);
-					}
-						return Math.max(...storage);		
+	this.SmallestSide = function () {
+		return this.minLength();
+	}
+	this.LongestSide = function () {
+		return this.maxLenght();
 					};
 };	
 
-class Camera {
-	constructor() {
+function Camera() {
 		this.focal;
 		this.pNumberX;
 		this.pNumberY;
 		this.sensorX;
 		this.sensorY;
 		this.recordingRate;
-	}
 }
 
 /*
@@ -66,7 +93,7 @@ Function to calculate respective smaller side of the image;
 arguments: longest side of picture, pixelnumber vertical, pixelnumber horizontal;
 return: smallest side of picture;
 */
-function calcSmallerSide(longestSideOfImage, pixelnumber_y, pixelnumber_x) {
+function calcSmallerSide(longestSideOfImage, pixelnumber_x, pixelnumber_y) {
 		if (pixelnumber_x > pixelnumber_y) {
 			var bigSide = pixelnumber_x
 			var smallSide = pixelnumber_y
@@ -100,8 +127,8 @@ arguments: shortest side of flight area, maximum vertical length of image, verti
 return: number of necessary vertical pictures
 */
 function calcNumberOfVerticalImages(ShortDistanceAOI, maxHeightOfImage, v_Overlap) {
-	checkValVertical = ShortDistanceAOI - maxHeightOfImage*0.5
-	counterVertical = 1
+	var checkValVertical = ShortDistanceAOI - maxHeightOfImage*0.5
+	var counterVertical = 1
 		while (checkValVertical >= (maxHeightOfImage/2)) {
 			checkValVertical -= maxHeightOfImage*(1-v_Overlap)
 			counterVertical += 1
@@ -115,14 +142,10 @@ Calculation of drone velocity in m/s with a certain recording rate
 arguments: recording rate, overlap horizontal, overlap vertical, vertical length of picture, horizontal length of picture
 return: vertical speed, horizontal speed
 */
-function calcVelocity(recRate,overlap_h,overlap_v,maxHeightOfImage, maxLengthOfImage) {
-	console.log("MaxHeight: "+maxHeightOfImage);
-	console.log("MaxLength: "+maxLengthOfImage);
-	var vertical = (maxHeightOfImage*(1-overlap_v))/2;
-	var horizontal = (maxLengthOfImage*(1-overlap_h))/2;
-	document.getElementById("velo_h").value = "Horizontal speed: " + Math.round((horizontal/recRate)*10)/10;
-	document.getElementById("velo_v").value = "Vertical speed: " + Math.round((vertical/recRate)*10)/10;
-	return [vertical, horizontal]
+function calcVelocity(recRate,overlap,side) {
+	var length = (side*(1-overlap))/2;
+	var speed = Math.round((length/recRate)*10)/10;
+	return speed;
 }
 
 function calcGSDFromHeight(camera,height) {
@@ -136,5 +159,5 @@ function calcGSDFromHeight(camera,height) {
 
 function calcHeightFromGSD(camera,gsd) {
 	var height = (gsd*camera.focal*camera.pNumberX)/camera.sensorX;
-	return console.log(height);
+	return height;
 }
